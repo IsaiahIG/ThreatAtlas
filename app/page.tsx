cb047604b09f42e66f3898f55d4ec2810bf78daa
Isaiah Ghansam <isaiahghansam2@gmail.com>
2026-03-04 19:01:25 -0500

    design v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,84 @@
-import Image from "next/image";
+"use client"
+import { FaBookDead } from "react-icons/fa";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+export default function Malwarescan(){
+
+    return (
+
+          <div className="absolute inset-0 -z-15 h-full w-full bg-black-800 bg-[linear-gradient(to_right,#272727_1px,transparent_1px),linear-gradient(to_bottom,#272727_1px,transparent_1px)] bg-size-[100px_50px]">
+            
+            <div className="max-w-7xl bg- mx-auto pt-20">
+              
+              <div className="space-y-15 pt-8">
+
+            
+              <div className = " flex items-center  justify-center ">  
+                
+                <div className=" space-y-10 flex justify-center items-center p-3 ">
+
+                  <div className="flex-col">
+
+                  <div className="flex p-2 justify-center space-x-6 ">
+                    
+                    <span>                    <FaBookDead className="size-15 text-[#196adc]"/>
+</span>
+
+                  
+
+                    <h1 className=" text-mono text-center tracking-widest text-5xl text-white  ">  Threat <span className="
+               
+                    text-[#196adc] font-bold  tracking-widest ">Atlas</span> 
+                
+                 </h1>
+
+                   
+                  
+                  
+                 </div>
+
+                 
+                 <p className="text-sm text-center font-mono tracking-widest  pt-8">   Analyse suspicious files, domains, IPs and URLs <span> <br/> to detect malware and other breaches</span> 
+                    </p>
+  
+</div>
+             
+                
+                
+                
+                </div>
+
+                
+                
+                
+                  </div>
+
+              
+
+                    </div>
+
+            
+            
+            
+            </div>
+
+                  
+                </div>
+
+
+
+                  
+
+
+
+    
+            
+
+
+
+
+    );
+
+
+
 }
+
